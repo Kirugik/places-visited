04d39c9e1ec8acc303ecb892b3b75f5ece93938d
Robert Kirui <kirugik79@gmail.com>
2022-03-15 15:10:38 +0300

    Add UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,43 @@
 //business logic 
 function PlaceVisited (name, location, timeOfYear, landmarks, accompaniedBy, notes) {
-    this.nameOfPlace = name;
+    this.name = name;
     this.location = location;
     this.timeOfYear = timeOfYear;
-    this.landmarksFound = landmarks;
-    this.accompaniedBy = accompaniedBy;
-    this.importantNotes = notes;
+    this.landmarks = landmarks;
+}
+
+PlaceVisited.prototype.placeDetails = function () {
+    return this.name + "" + this.location + "" + this.timeOfYear + "" + this.landmarks;
 }
 
 
 //UI logic 
+$(document).ready(function() {
+    $("form#new-place").submit(function(event) {
+        event.preventDefault();
+
+        let inputtedName = $("input#new-place-visited").val();
+        let inputtedLocation = $("input#new-place-location").val();
+        let timeOftheYear = $("input#time-of-year").val();
+        let majorLandmarks = $("input#landmarks").val();
+
+        let newPlaceVisited = new PlaceVisited(inputtedName, inputtedLocation, timeOftheYear, majorLandmarks);
+
+        $("ul#list").append ("<li><span class='place'>" + newPlaceVisited.placeDetails() + "</span></li>");
+
+        $(".place").name().click(function() {
+            $("#show-place-visited").show();
+            $("#show-place-visited h2").text(newPlaceVisited.name);
+            $(".place-name").text(newPlaceVisited.name);
+            $(".place-location").text(newPlaceVisited.location);
+            $(".time-of-year").text(newPlaceVisited.timeOfYear);
+            $(".place-landmarks").text(newPlaceVisited.landmarks);
+          });
+
+        $("input#new-place-visited").val();
+        $("input#new-place-location").val(); 
+        $("input#time-of-year").val();
+        $("input#landmarks").val();
+
+    });
+});
