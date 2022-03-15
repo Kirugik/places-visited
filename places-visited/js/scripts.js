9d69678ddbe1993cd29cd3bf95fdfe5a3fa49370
Robert Kirui <kirugik79@gmail.com>
2022-03-15 13:42:11 +0300

    Add webpage structure and business logic

---
+++ places-visited/js/scripts.js
@@ -0,0 +1,12 @@
+//business logic 
+function PlaceVisited (name, location, timeOfYear, landmarks, accompaniedBy, notes) {
+    this.nameOfPlace = name;
+    this.location = location;
+    this.timeOfYear = timeOfYear;
+    this.landmarksFound = landmarks;
+    this.accompaniedBy = accompaniedBy;
+    this.importantNotes = notes;
+}
+
+
+//UI logic 
